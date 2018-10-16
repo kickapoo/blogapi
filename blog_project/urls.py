0109c095898ac6c277a1183148744a58f1007d67
Stavros Anastasiadis <anastasiadis.st00@gmail.com>
2018-10-16 12:58:34 +0300

    Add schema endpoint support with coreapi

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.schemas import get_schema_view
 
+schema_view = get_schema_view(title='Simple Blog API')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,4 +10,5 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')), # Add Log In in Browsable API
     path('api/v1/rest-auth/', include('rest_auth.urls')), # Login/Logout/Password Reset/Password Reset Confirm
     path('api/v1/rest-auth/registration', include('rest_auth.registration.urls')), # Registration Urls
+    path('api/schema/', schema_view)
 ]
