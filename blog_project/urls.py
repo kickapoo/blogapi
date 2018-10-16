94f2fdf8e4bb853d5f2ac88f74a89c802a41edb2
Stavros Anastasiadis <anastasiadis.st00@gmail.com>
2018-10-16 12:21:27 +0300

    Add rest-auth for Login/Logout/Password Reset and Confirm endpoints

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('posts.urls')),
     path('api-auth/', include('rest_framework.urls')), # Add Log In in Browsable API
+    path('api/v1/rest-auth/', include('rest_auth.urls')), # Login/Logout/Password Reset/Password Reset Confirm
 ]
