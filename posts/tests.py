59783006646122b0498bb4b3df2831f40791a063
Stavros Anastasiadis <anastasiadis.st00@gmail.com>
2018-10-16 11:31:54 +0300

    Add Post Model Unit Test

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from .models import Post
+
+class BlogPostTest(TestCase):
+
+    @classmethod
+    def setUpTestData(self):
+        testuser = User.objects.create_user(
+            username='testuser',
+            password='123'
+        )
+        testuser.save()
+
+        testpost = Post.objects.create(
+            author=testuser,
+            title="This is a title",
+            body="This is a body",
+        )
+        testpost.save()
+
+    def test_post_content(self):
+        post = Post.objects.get(pk=1)
+        self.assertEquals(f'{post.author}', 'testuser')
+        self.assertEquals(f'{post.title}', 'This is a title')
+        self.assertEquals(f'{post.body}', 'This is a body')
