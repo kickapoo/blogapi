d3a47d14570df3df9e4c9144a408157a574bcd67
Stavros Anastasiadis <anastasiadis.st00@gmail.com>
2018-10-16 13:16:00 +0300

    Add docs endpoint

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.schemas import get_schema_view
+from rest_framework.documentation import include_docs_urls
 
-schema_view = get_schema_view(title='Simple Blog API')
+API_TITLE = 'Simple Blog API'
+API_DESCRIPTION = 'A simple Blog API to enchant my DRF learing curve'
+
+schema_view = get_schema_view(title=API_TITLE)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,5 +14,6 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')), # Add Log In in Browsable API
     path('api/v1/rest-auth/', include('rest_auth.urls')), # Login/Logout/Password Reset/Password Reset Confirm
     path('api/v1/rest-auth/registration', include('rest_auth.registration.urls')), # Registration Urls
-    path('api/schema/', schema_view)
+    path('api/docs', include_docs_urls(title=API_TITLE, description=API_DESCRIPTION)),
+    path('api/schema/', schema_view),
 ]
